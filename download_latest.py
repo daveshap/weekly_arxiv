6b2a84b3ceb089a07af38db8cd45c667f26b962c
David Shapiro <daveshap37@gmail.com>
2023-10-05 10:09:46 -0400

    all done

--- download_latest.py
+++ download_latest.py
@@ -1,8 +1,14 @@
 import requests
+from time import time
 from xml.etree import ElementTree as ET
 
+
+# get search term
+a = input("Whatcha wanna lookup? ")
+
+
 # URL of the XML object
-url = "https://export.arxiv.org/api/query?search_query=all:llms&sortBy=lastUpdatedDate&sortOrder=descending&max_results=200"
+url = "https://export.arxiv.org/api/query?search_query=all:%s&sortBy=lastUpdatedDate&sortOrder=descending&max_results=200" % a.lower().replace(' ','%20')
 
 # Send a GET request to the URL
 response = requests.get(url)
@@ -14,7 +20,7 @@ root = ET.fromstring(response.content)
 namespaces = {'atom': 'http://www.w3.org/2005/Atom', 'arxiv': 'http://arxiv.org/schemas/atom'}
 
 # Open the output file with UTF-8 encoding
-with open("output.md", "w", encoding='utf-8') as file:
+with open("output-%s-%s.md" % (time(), a), "w", encoding='utf-8') as file:
     # Iterate over each entry in the XML data
     for entry in root.findall('atom:entry', namespaces):
         # Extract and write the title
